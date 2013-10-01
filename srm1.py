c5c0a22e734ef4800d36d9792a76e5f9b96235d2
Jose Luis Romero <tangun73@gmail.com>
2013-09-30 21:09:23 -0430

    added method get_x_smallest

--- srm1.py
+++ srm1.py
@@ -9,3 +9,18 @@ class Finances():
 			if time >= 1200:
 				return time
 		return time
+
+
+class Fraction():
+	def get_x_smallest(self, x, a):
+		aux_index = 0
+		arr = [float(1) for y in range(1, 10001)]
+		for i in range(1, a + 1):
+			for j in range(1, i):
+				val = float(j) / float(i)
+				aux_index = aux_index + 1
+				arr[aux_index] = val
+		arr.sort()
+		return arr[x - 1]
+
+
